cfe2c334b4ee5646b51b072d0269b6007c70f3d8
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-11-22 14:36:24 +0330

    Featured Products added to home page

---
+++ src/api/products.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const URL = "https://fakestoreapi.com";
+
+export const featuredProducts = async () => {
+    const res = await axios.get(`${URL}/products?limit=4`);
+    return res.data
+}
+
+export const allProducts = async () => {
+  const res = await axios.get(`${URL}/products`);
+  return res.data;
+};
+
+export const productsDetails = async (id) => {
+    const res = await axios.get(`${URL}/products/${id}`);
+    return res.data;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,64 @@
-import Hero from "../components/Hero"
-
+import Hero from "../components/Hero";
+import { useQuery } from "@tanstack/react-query";
+import { featuredProducts } from "../api/products";
+import { Link } from "react-router-dom";
 const Home = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: featuredProducts,
+    queryKey: ["featuredProducts"],
+  });
   return (
     <div>
       <Hero />
-    </div>
-  )
-}
+      <section className="p-10">
+        <h2 className="text-3xl font-bold text-blue-400 mb-8 text-center">
+          Featured Products
+        </h2>
 
-export default Home
+        {isLoading ? (
+          <p className="text-center text-blue-300">Loading products...</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            {data.map((product) => (
+              <div
+                key={product.id}
+                className="
+                  bg-gray-800 
+                  p-4 
+                  rounded-xl 
+                  shadow-lg 
+                  flex flex-col items-center 
+                  hover:scale-105 
+                  transition 
+                  h-[360px] 
+                  justify-between
+                "
+              >
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="h-40 object-contain"
+                />
+
+                <h3 className="font-semibold text-center line-clamp-2">
+                  {product.title}
+                </h3>
+
+                <p className="text-green-400 font-bold">${product.price}</p>
+
+                <Link
+                  to={`/products/${product.id}`}
+                  className="bg-blue-600 hover:bg-blue-500 px-4 py-2 rounded-lg text-white"
+                >
+                  View Details
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default Home;
